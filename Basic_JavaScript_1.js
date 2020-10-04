c2863e0af62fd1ee5473e1ce1999b67ed586a5c6
Neemag97 <71804843+Neemag97@users.noreply.github.com>
2020-10-04 01:30:36 -0700

    Initial commit

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+<script>
+	Function My_First_Function(){
+		var string = "Congratulate me, I'm a student!";
+		var result = string.fontcolor("green");
+		document.getElementByld("Student").innerHTML = result;
+	}
+</script>
